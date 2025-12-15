23bbaecd6f95118b5142cb3e5ec6aed308b4f97b
Maikel Joel de Souza <maikel.souza@gmail.com>
2025-12-15 20:41:06 -0300

    feat: displaying a message when there are no transactions

---
+++ src/app/features/home/components/no-transactions/no-transactions.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatCard, MatCardMdImage, MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-no-transactions',
+  imports: [MatCardModule],
+  templateUrl: './no-transactions.html',
+  styleUrl: './no-transactions.scss',
+})
+export class NoTransactions {
+
+}
--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -3,19 +3,20 @@ import { Balance } from "./components/balance/balance";
 import { TransactionItem } from './components/transaction-item/transaction-item';
 import { Transaction } from '../../shared/transaction/interfaces/transaction';
 import { TransactionType } from '../../shared/transaction/interfaces/enums/transaction-type';
+import { NoTransactions } from './components/no-transactions/no-transactions';
 
 @Component({
   selector: 'app-home',
-  imports: [Balance, TransactionItem],
+  imports: [Balance, TransactionItem, NoTransactions],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
 
  transactions = signal<Transaction[]>([
-    { title: 'Salário', value: 100, type: TransactionType.INCOME},
-    { title: 'VA', value: 50, type: TransactionType.INCOME},
-    { title: 'Aluguel', value: 150, type: TransactionType.OUTCOME},
+    // { title: 'Salário', value: 100, type: TransactionType.INCOME},
+    // { title: 'VA', value: 50, type: TransactionType.INCOME},
+    // { title: 'Aluguel', value: 150, type: TransactionType.OUTCOME},
     
   ]);
 
