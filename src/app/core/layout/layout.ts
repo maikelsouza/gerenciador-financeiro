9b778217801338deeec80860ce133fe3fbb4954d
Maikel Joel de Souza <maikel.souza@gmail.com>
2026-01-16 08:45:01 -0300

    feat: using mat-nav-list to display items in the sidenav

--- src/app/core/layout/layout.ts
+++ src/app/core/layout/layout.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Header } from './components/header/header';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatList, MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-layout',
-  imports: [Header, RouterOutlet],
+  imports: [Header, RouterOutlet, MatSidenavModule, 
+            MatListModule, RouterLink, RouterLinkActive],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
 export class Layout {
 
+  links = signal([
+    {
+      label: 'Home',
+      url: '/'
+    }    
+  ]);
 }
