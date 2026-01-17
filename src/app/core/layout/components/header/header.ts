f5aca8b2e349076c12b452b0ba0f7eb697805062
Maikel Joel de Souza <maikel.souza@gmail.com>
2026-01-17 10:31:44 -0300

    feat: moving logout action into Sidenav

--- src/app/core/layout/components/header/header.ts
+++ src/app/core/layout/components/header/header.ts
@@ -1,9 +1,6 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { LogoutFacadeService } from '../../../auth/facades/logout-facade.service';
-import { Router } from '@angular/router';
-import { LoggedInUserStoreService } from '../../../auth/stores/logged-in-user-store.service';
 
 @Component({
   selector: 'app-header',
@@ -14,22 +11,6 @@ import { LoggedInUserStoreService } from '../../../auth/stores/logged-in-user-st
 export class Header {
 
 
- private readonly logoutFacadeService = inject(LogoutFacadeService);
-
- private readonly router = inject(Router);
-
- private readonly loggedInUserStoreService = inject(LoggedInUserStoreService);
-
-isLoggedIn = computed(() => this.loggedInUserStoreService.isLoggedIn());
-
-
-  logout() {
-    this.logoutFacadeService.logout()
-      .subscribe({
-        next: () => {   
-          this.router.navigate(['/auth/login']);
-        }
-      }); 
-  } 
+ 
 
 }
--- src/app/core/layout/components/sidenav-items/sidenav-items.component.ts
+++ src/app/core/layout/components/sidenav-items/sidenav-items.component.ts
@@ -1,20 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LoggedInUserStoreService } from '../../../auth/stores/logged-in-user-store.service';
+import { LogoutDirective } from './directives/logout.directive';
 
 @Component({
   selector: 'app-sidenav-items',
-  imports: [RouterLink, RouterLinkActive, MatListModule],
+  imports: [RouterLink, RouterLinkActive, MatListModule, LogoutDirective],
   templateUrl: './sidenav-items.component.html',
   styleUrl: './sidenav-items.component.scss',
 })
 export class SidenavItemsComponent {
 
-   links = signal([
+ private readonly loggedInUserStoreService = inject(LoggedInUserStoreService);
+
+ isLoggedIn = computed(() => this.loggedInUserStoreService.isLoggedIn());
+
+  links = signal([
     {
       label: 'Home',
       url: '/'
     }    
   ]);
+
+ 
+
 
 }
