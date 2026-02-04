b444712a9bcdfc828c0597da178be4d9de77cda7
Maikel Joel de Souza <maikel.souza@gmail.com>
2026-02-04 08:47:35 -0300

    feat: feeding the chart with real data

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -3,6 +3,7 @@ import { Transaction } from '@shared/transaction/interfaces/transaction';
 import { Balance } from './components/balance/balance';
 import { PieChartComponent } from './components/pie-chart/pie-chart.component';
 import { PieChartConfig } from './components/pie-chart/pie-chart-config.interface';
+import { TransactionType } from '@shared/transaction/interfaces/enums/transaction-type';
 
 
 @Component({
@@ -13,10 +14,24 @@ import { PieChartConfig } from './components/pie-chart/pie-chart-config.interfac
 })
 export class HomeComponent{
 
-  transactions = input.required<Transaction[]>()
+ transactions = input.required<Transaction[]>()
+
+ totalIncomes = computed(() => {
+    return this.transactions()
+    .filter((item) => item.type === TransactionType.INCOME)
+    .reduce((total, item) => total + item.value, 0);
+  });
+
+  totalOutcomes = computed(() => {
+    return this.transactions()
+    .filter((item) => item.type === TransactionType.OUTCOME)
+    .reduce((total, item) => total + item.value, 0);
+  });
 
  chartConfig = computed<PieChartConfig>(() => {
-    return {labels: ['teste'], dataLabel: 'Teste', data: [100]}
+    return {labels: ['Ganhos', 'Gastos'],
+           dataLabel: 'Valor Total',
+           data: [this.totalIncomes(), this.totalOutcomes()]}
  });
   
 
