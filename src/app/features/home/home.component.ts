28d2739a992d99bd49fc3d62118051e06a4c9669
Maikel Joel de Souza <maikel.souza@gmail.com>
2026-02-12 08:54:47 -0300

    feat: improving graph rendering styling

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,20 +1,27 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
 import { Transaction } from '@shared/transaction/interfaces/transaction';
 import { Balance } from './components/balance/balance';
 import { PieChartComponent } from './components/pie-chart/pie-chart.component';
 import { PieChartConfig } from './components/pie-chart/pie-chart-config.interface';
 import { TransactionType } from '@shared/transaction/interfaces/enums/transaction-type';
 import { sumTransactions } from '@shared/transaction/functions/sum-transactions';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-   imports: [Balance, PieChartComponent],
+   imports: [Balance, PieChartComponent, MatCardModule, 
+    MatButtonModule, MatProgressBarModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent{
 
  transactions = input.required<Transaction[]>()
+
+ canLoudComponent = signal(false)
 
  totalIncomes = computed(() => {
     return sumTransactions(this.transactions(), TransactionType.INCOME);
